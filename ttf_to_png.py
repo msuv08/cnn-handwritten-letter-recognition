f8a0366d66b4e6fb95ea113329499af8d579fc1d
Mihir Suvarna <49384703+msuv08@users.noreply.github.com>
2022-05-01 20:34:35 -0500

    TTF_TO_PNG script finished for individual fonts

--- ttf_to_png.py
+++ ttf_to_png.py
@@ -2,22 +2,24 @@
 import sys
 import os
 import subprocess
+import shutil
+import glob
 
+from PIL import Image
 from fontTools.ttLib import TTFont
 
 TEXTS_DIR = "texts"
 IMAGES_DIR = "images"
-
+# Get path using sys args, set up font loader
 TTF_PATH = sys.argv[1]
 FONT_SIZE = sys.argv[2]
 TTF_NAME, TTF_EXT = os.path.splitext(os.path.basename(TTF_PATH))
-
 ttf = TTFont(TTF_PATH, 0, verbose=0, allowVID=0, ignoreDecompileErrors=True, fontNumber=-1)
-
+# Make temporary directories
 for d in [TEXTS_DIR, IMAGES_DIR]:
     if not os.path.isdir(d):
         os.mkdir(d)
-
+# Writing each individual font character to file as texts
 for x in ttf["cmap"].tables:
     for y in x.cmap.items():
         char_unicode = chr(y[0])
@@ -27,7 +29,7 @@ for x in ttf["cmap"].tables:
         f.write(char_utf8)
         f.close()
 ttf.close()
-
+# Writing each text as a png through converting it
 files = os.listdir(TEXTS_DIR)
 for filename in files:
     name, ext = os.path.splitext(filename)
@@ -36,3 +38,31 @@ for filename in files:
     subprocess.call(["convert", "-font", TTF_PATH, "-pointsize", FONT_SIZE, "label:@" + input_txt, output_png])
 
 print("TTF -> Image Conversion Finished")
+
+# Normalizing the images to all be of the correct size
+src_dir = 'images'
+dst_dir = 'cropped_images'
+thumb_width = 28
+
+files = glob.glob(os.path.join(src_dir, '*.png'))
+
+for f in files:
+    im = Image.open(f)
+    im_thumb = im.resize((thumb_width, thumb_width), Image.LANCZOS)
+    ftitle, fext = os.path.splitext(os.path.basename(f))
+    # Ommitting all non-readable UTF characters
+    if "CR" in ftitle:
+        continue
+    elif "nonmarkingreturn" in ftitle:
+        continue
+    elif "nbsp" in ftitle:
+        continue
+    elif "space" in ftitle:
+        continue
+    else:
+        im_thumb.save(os.path.join(dst_dir, ftitle + '_thumbnail' + fext), quality=95)
+        
+print("Images -> Cropped Images Finished")
+# Deleting temporary directories
+for d in [TEXTS_DIR, IMAGES_DIR]:
+    shutil.rmtree(d)
